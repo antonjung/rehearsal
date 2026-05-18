68bd07c09ce8a72556387580b18382c262063be8
JUNGLEMAJOR1\anton <anton@junglemajor.co.uk>
2026-05-18 13:39:35 +0100

    build: v2.37.0

--- src/components/RehearsalMode.tsx
+++ src/components/RehearsalMode.tsx
@@ -490,7 +490,10 @@ export function RehearsalMode({ onExit }: Props) {
       {/* Show/hide my lines toggle */}
       <div className="px-4 py-2 border-b border-[var(--color-stage-border)] shrink-0 flex items-center justify-between">
         <span className="text-xs text-[var(--color-stage-text)]">Show all my lines</span>
-        <ToggleSwitch checked={showAllMyLines} onChange={setShowAllMyLines} />
+        <ToggleSwitch checked={showAllMyLines} onChange={(v) => {
+          setShowAllMyLines(v)
+          setRevealedLines({})
+        }} />
       </div>
 
       {/* Script area */}
